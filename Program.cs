a453e24a847925f88be98b09f0cbc03a3bfc0bae
NANCY-HUB329 <mwanginancy333@gmail.com>
2023-12-06 08:36:24 +0300

    A c# program to find sum

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Addition
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int sum = AddNumbers(a, b);
+
+            Console.WriteLine($"The sum of {a} and {b} is: {sum}");
+        }
+
+        public static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
